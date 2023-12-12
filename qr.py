97fe446f4b3165ce634e7de20b6f3365be082b31
t-kanetake <152838449+t-kanetake@users.noreply.github.com>
2023-12-12 10:35:26 -0800

    Add files via upload

---
+++ qr.py
@@ -0,0 +1,24 @@
+# project: qr code
+
+import qrcode
+
+# creates a function that calls the qrcode class
+def generate_qr(text):
+
+  #outlines qr code format
+  qr = qrcode.QRCode(
+    version = 1,
+    error_correction = qrcode.constants.ERROR_CORRECT_L,
+    box_size = 10,
+    border = 4,
+  )
+
+  # adds data to qr code and saves it as a png file
+  qr.add_data(text)
+  qr.make(fit=True)
+  img = qr.make_image(fill_color = "black", back_color = "white")
+  img.save("qrimage.png")
+
+# asks for input of url and calls the function
+url = input("Enter your url: ").strip()
+generate_qr(url)
