b9df206f83d81d78be60b54ff1abd4a5b6477f7a
t-kanetake <152838449+t-kanetake@users.noreply.github.com>
2023-12-12 12:28:48 -0800

    Add files via upload

---
+++ randpassword.py
@@ -0,0 +1,37 @@
+# project: random password generator
+
+import string
+import random
+
+characters = list(string.ascii_letters + string.digits + "!@#$%&")
+
+def generate_password():
+  password_length = int(input("How long would you like your password to be? "))
+
+  random.shuffle(characters)
+  
+  password = []
+
+  for _ in range(password_length):
+    password.append(random.choice(characters))
+
+  random.shuffle(password)
+
+  password = "".join(password)
+
+  print(password)
+
+def ask():
+  option = input("Do you want to generate a password? (Yes/No): ")
+ 
+  if option.strip().lower() in ["yes", "y"]:
+    generate_password()
+  elif option.strip().lower() in ["no", "n"]:
+    print("Program ended")
+    quit()
+  else:
+    print("Invalid input. Please try again.")
+    ask()
+
+if __name__ == "__main__":
+  ask()
