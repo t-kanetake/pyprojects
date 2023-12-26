9fa663f143f01f7daed526c95ea256d4fd1e9389
Tetsuhiro K. <tsbkanetake@gmail.com>
2023-12-26 12:41:17 -0800

    Add game files for space invaders game

---
+++ games/spaceinvaders/player.py
@@ -0,0 +1,9 @@
+# player class
+
+import pygame
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self, pos):
+        super().__init__()
+        self.image = pygame.image.load("spaceinvaders/graphics/player.png").convert_alpha
+        self.rect = self.image.get_rect(midbottom = pos)
--- games/spaceinvaders/spaceinvaders.py
+++ games/spaceinvaders/spaceinvaders.py
@@ -2,6 +2,17 @@
 # created: 12/26/2023
 
 import pygame, sys
+from player import Player
+
+class Game:
+    def __init__(self):
+        player_sprite = Player((300, 300))
+        self.player = pygame.sprite.GroupSingle(player_sprite)
+
+    def run(self):
+        self.player.draw(screen)
+
+
 
 # pygame initialization
 pygame.init()
@@ -11,11 +22,13 @@ screen_width = 600
 screen_height = 600
 screen = pygame.display.set_mode((screen_width, screen_height))
 
-# framerate class
+# variables for game functionality
 clock = pygame.time.Clock()
+game = Game()
 
 # event loop
 while True:
+    # exit game without throwing an error
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
@@ -23,6 +36,8 @@ while True:
 
     # color for bg
     screen.fill((30, 30, 30))
+    game.run()
 
+    # draw bg and initialize frame rate
     pygame.display.flip()
     clock.tick(60)
