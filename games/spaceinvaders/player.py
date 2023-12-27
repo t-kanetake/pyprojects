8dbf0ae3a1012fd94e567265b7664756e9f2024c
Tetsuhiro K. <tsbkanetake@gmail.com>
2023-12-27 09:40:53 -0800

    Remove spaceinvaders

--- games/spaceinvaders/player.py
+++
@@ -1,9 +0,0 @@
-# player class
-
-import pygame
-
-class Player(pygame.sprite.Sprite):
-    def __init__(self, pos):
-        super().__init__()
-        self.image = pygame.image.load("spaceinvaders/graphics/player.png").convert_alpha
-        self.rect = self.image.get_rect(midbottom = pos)
